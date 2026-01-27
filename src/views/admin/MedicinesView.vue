c059df1b7d8b5146cbe47887d38d7b712b83447e
Ferdinand05 <fkoryanto@gmail.com>
2026-01-27 16:49:25 +0700

    refactor(medicine) : make some element to reuse components

--- src/views/admin/MedicinesView.vue
+++ src/views/admin/MedicinesView.vue
@@ -12,7 +12,6 @@ import type { ICategory, IMedicine } from '@/types/medicine'
 import { onMounted } from 'vue'
 import {
   createColumnHelper,
-  FlexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
@@ -20,7 +19,6 @@ import {
   useVueTable,
   type SortingState,
 } from '@tanstack/vue-table'
-import { ArrowUpDown, SortAsc, SortDesc } from 'lucide-vue-next'
 import type { IUser } from '@/types/user.type'
 import type { errorForm } from '@/types'
 import SelectCategoryComponent from '@/components/medicine/SelectCategoryComponent.vue'
@@ -28,6 +26,9 @@ import { getAllUsers } from '@/services/api/admin/users.api'
 import Swal from 'sweetalert2'
 import AlertComponent from '@/components/AlertComponent.vue'
 import { useAuthStore } from '@/stores/auth'
+import TableData from '@/components/table/TableData.vue'
+import HeaderDashboard from '@/components/HeaderDashboard.vue'
+import ModalDialog from '@/components/ModalDialog.vue'
 
 const medicines = ref<IMedicine[]>([])
 function getData() {
@@ -112,7 +113,7 @@ const columns = [
       columnHelper.accessor((row) => (row.category as ICategory)?.name, {
         id: 'category',
         header: () => 'Category',
-        cell: (info) => info.getValue() ?? '-',
+        cell: (info) => info.getValue(),
       }),
 
       // ===== EXPIRE DATE =====
@@ -170,7 +171,7 @@ const columns = [
 const pageSizes = [10, 20, 30, 40, 50]
 const globalFilter = ref('')
 const sorting = ref<SortingState>([])
-const table = useVueTable({
+const table = useVueTable<IMedicine>({
   get data() {
     return medicines.value
   },
@@ -197,17 +198,7 @@ const table = useVueTable({
   getPaginationRowModel: getPaginationRowModel(),
 })
 
-function handlePageSizeChange(e: Event) {
-  const target = e.target as HTMLSelectElement | HTMLInputElement | null
-  if (!target) return
-  table.setPageSize(Number(target.value))
-}
-
 const modalCreate = ref()
-function handleCreate() {
-  errorCreate.value = {}
-  modalCreate.value?.showModal()
-}
 
 function handleDelete(id: string) {
   Swal.fire({
@@ -349,14 +340,7 @@ function update() {
 <template>
   <DashboardAdminLayout>
     <section class="p-4 space-y-5">
-      <div class="">
-        <div>
-          <h1 class="text-2xl font-semibold">Medicines</h1>
-          <p class="font-light text-lg">Manage medicines safely</p>
-        </div>
-      </div>
-
-      <div class="flex md:justify-between gap-3 items-center w-full flex-col md:flex-row">
+      <HeaderDashboard title="Medicines" :modal="modalCreate">
         <div class="w-full md:w-auto">
           <label class="input">
             <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -374,255 +358,156 @@ function update() {
             <input type="search" required placeholder="Search" v-model="globalFilter" />
           </label>
         </div>
-        <div>
-          <button class="btn btn-success btn-md" @click="handleCreate()">
-            Create new Medicine
-          </button>
-        </div>
-      </div>
+      </HeaderDashboard>
 
       <!-- table data -->
-      <div class="overflow-x-auto">
-        <table class="table table-zebra">
-          <thead>
-            <tr v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-              <th v-for="header in headerGroup.headers" :key="header.id" :colSpan="header.colSpan">
-                <button
-                  v-if="header.column.getCanSort()"
-                  class="flex items-center gap-1 font-semibold"
-                  @click="header.column.toggleSorting()"
-                >
-                  <FlexRender
-                    :render="header.column.columnDef.header"
-                    :props="header.getContext()"
-                  />
-
-                  <!-- SORT ICON -->
-                  <span class="text-sm">
-                    <template v-if="header.column.getIsSorted() === 'asc'"
-                      ><SortAsc :size="15" class="cursor-pointer"
-                    /></template>
-                    <template v-else-if="header.column.getIsSorted() === 'desc'">
-                      <SortDesc :size="15" class="cursor-pointer" />
-                    </template>
-                    <template v-else> <ArrowUpDown :size="15" class="cursor-pointer" /> </template>
-                  </span>
-                </button>
-
-                <!-- HEADER TANPA SORT -->
-                <FlexRender
-                  v-else
-                  :render="header.column.columnDef.header"
-                  :props="header.getContext()"
-                />
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr v-for="row in table.getRowModel().rows" :key="row.id">
-              <td v-for="cell in row.getVisibleCells()" :key="cell.id">
-                <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <div class="flex justify-between items-center p-3">
-          <div class="w-full">
-            <select
-              class="input select select-xs w-28"
-              :value="table.getState().pagination.pageSize"
-              @change="handlePageSizeChange"
-            >
-              <option :key="pageSize" :value="pageSize" v-for="pageSize in pageSizes">
-                Show {{ pageSize }}
-              </option>
-            </select>
-          </div>
-          <div class="w-sm">
-            <div class="join grid grid-cols-2">
-              <button
-                class="join-item btn btn-outline btn-sm"
-                @click="() => table.previousPage()"
-                :disabled="!table.getCanPreviousPage()"
-              >
-                Previous
-              </button>
-              <button
-                class="join-item btn btn-outline btn-sm"
-                @click="() => table.nextPage()"
-                :disabled="!table.getCanNextPage()"
-              >
-                Next
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TableData :table="table" :pageSizes="pageSizes" />
     </section>
   </DashboardAdminLayout>
 
   <!-- modal create -->
-  <dialog ref="modalCreate" class="modal">
-    <div class="modal-box">
-      <form method="dialog">
-        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-      </form>
-      <h3 class="text-lg font-bold">Create new</h3>
-      <form @submit.prevent="store()">
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Name</legend>
-          <input
-            v-model="createForm.name"
-            type="text"
-            class="input w-full"
-            placeholder="Medicine name"
-          />
-          <AlertComponent v-if="errorCreate?.name">
-            {{ errorCreate.name[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Quantity</legend>
-          <input
-            type="number"
-            v-model="createForm.quantity"
-            class="input w-full"
-            placeholder="Qty"
-          />
-          <AlertComponent v-if="errorCreate?.quantity">
-            {{ errorCreate.quantity[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Unit</legend>
-          <input type="text" class="input w-full" v-model="createForm.unit" placeholder="Unit" />
-          <p class="label">"strip","tablet","bottle","ml"</p>
-          <AlertComponent v-if="errorCreate?.unit">
-            {{ errorCreate.unit[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Expire Date</legend>
-          <input
-            type="date"
-            class="input w-full"
-            placeholder="Expire date"
-            v-model="createForm.expireDate"
-          />
-          <AlertComponent v-if="errorCreate?.expireDate">
-            {{ errorCreate.expireDate[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Note</legend>
-          <input type="text" class="input w-full" placeholder="Note" v-model="createForm.note" />
-          <AlertComponent v-if="errorCreate?.note">
-            {{ errorCreate.note[0] }}
-          </AlertComponent>
-        </fieldset>
-        <!-- select category -->
-        <SelectCategoryComponent v-model="selectedCategoryId" />
-        <AlertComponent v-if="errorCreate?.category">
-          {{ errorCreate.category[0] }}
+  <ModalDialog for="Create" title="Medicine" ref="modalCreate">
+    <form @submit.prevent="store()">
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Name</legend>
+        <input
+          v-model="createForm.name"
+          type="text"
+          class="input w-full"
+          placeholder="Medicine name"
+        />
+        <AlertComponent v-if="errorCreate?.name">
+          {{ errorCreate.name[0] }}
         </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Quantity</legend>
+        <input type="number" v-model="createForm.quantity" class="input w-full" placeholder="Qty" />
+        <AlertComponent v-if="errorCreate?.quantity">
+          {{ errorCreate.quantity[0] }}
+        </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Unit</legend>
+        <input type="text" class="input w-full" v-model="createForm.unit" placeholder="Unit" />
+        <p class="label">"strip","tablet","bottle","ml"</p>
+        <AlertComponent v-if="errorCreate?.unit">
+          {{ errorCreate.unit[0] }}
+        </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Expire Date</legend>
+        <input
+          type="date"
+          class="input w-full"
+          placeholder="Expire date"
+          v-model="createForm.expireDate"
+        />
+        <AlertComponent v-if="errorCreate?.expireDate">
+          {{ errorCreate.expireDate[0] }}
+        </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Note</legend>
+        <input type="text" class="input w-full" placeholder="Note" v-model="createForm.note" />
+        <AlertComponent v-if="errorCreate?.note">
+          {{ errorCreate.note[0] }}
+        </AlertComponent>
+      </fieldset>
+      <!-- select category -->
+      <SelectCategoryComponent v-model="selectedCategoryId" />
+      <AlertComponent v-if="errorCreate?.category">
+        {{ errorCreate.category[0] }}
+      </AlertComponent>
 
-        <!-- select user -->
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Select User</legend>
-          <select class="select w-full" placeholder="Select user" v-model="createForm.user">
-            <option v-for="(user, i) in users" :key="i" :value="user._id">
-              {{ user.username }}
-            </option>
-          </select>
-          <AlertComponent v-if="errorCreate?.user">
-            {{ errorCreate.user[0] }}
-          </AlertComponent>
-        </fieldset>
-        <div class="mt-2 text-right">
-          <button type="submit" class="btn btn-success btn-sm">Create</button>
-        </div>
-      </form>
-    </div>
-  </dialog>
+      <!-- select user -->
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Select User</legend>
+        <select class="select w-full" placeholder="Select user" v-model="createForm.user">
+          <option v-for="(user, i) in users" :key="i" :value="user._id">
+            {{ user.username }}
+          </option>
+        </select>
+        <AlertComponent v-if="errorCreate?.user">
+          {{ errorCreate.user[0] }}
+        </AlertComponent>
+      </fieldset>
+      <div class="mt-2 text-right">
+        <button type="submit" class="btn btn-success btn-sm">Create</button>
+      </div>
+    </form>
+  </ModalDialog>
 
   <!-- modal edit -->
-  <dialog ref="modalEdit" class="modal">
-    <div class="modal-box">
-      <form method="dialog">
-        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-      </form>
-      <h3 class="text-lg font-bold">Edit</h3>
-      <form @submit.prevent="update()">
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Name</legend>
-          <input
-            v-model="editForm.name"
-            type="text"
-            class="input w-full"
-            placeholder="Medicine name"
-          />
-          <AlertComponent v-if="errorEdit?.name">
-            {{ errorEdit.name[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Quantity</legend>
-          <input type="number" v-model="editForm.quantity" class="input w-full" placeholder="Qty" />
-          <AlertComponent v-if="errorEdit?.quantity">
-            {{ errorEdit.quantity[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Unit</legend>
-          <input type="text" class="input w-full" v-model="editForm.unit" placeholder="Unit" />
-          <p class="label">"strip","tablet","bottle","ml"</p>
-          <AlertComponent v-if="errorEdit?.unit">
-            {{ errorEdit.unit[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Expire Date</legend>
-          <input
-            type="date"
-            class="input w-full"
-            placeholder="Expire date"
-            v-model="editForm.expireDate"
-          />
-          <AlertComponent v-if="errorEdit?.expireDate">
-            {{ errorEdit.expireDate[0] }}
-          </AlertComponent>
-        </fieldset>
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Note</legend>
-          <input type="text" class="input w-full" placeholder="Note" v-model="editForm.note" />
-          <AlertComponent v-if="errorEdit?.note">
-            {{ errorEdit.note[0] }}
-          </AlertComponent>
-        </fieldset>
-        <!-- select category -->
-        <SelectCategoryComponent v-model="selectedCategoryId" />
-        <AlertComponent v-if="errorEdit?.category">
-          {{ errorEdit.category[0] }}
+  <ModalDialog for="Edit" title="Medicine" ref="modalEdit">
+    <form @submit.prevent="update()">
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Name</legend>
+        <input
+          v-model="editForm.name"
+          type="text"
+          class="input w-full"
+          placeholder="Medicine name"
+        />
+        <AlertComponent v-if="errorEdit?.name">
+          {{ errorEdit.name[0] }}
         </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Quantity</legend>
+        <input type="number" v-model="editForm.quantity" class="input w-full" placeholder="Qty" />
+        <AlertComponent v-if="errorEdit?.quantity">
+          {{ errorEdit.quantity[0] }}
+        </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Unit</legend>
+        <input type="text" class="input w-full" v-model="editForm.unit" placeholder="Unit" />
+        <p class="label">"strip","tablet","bottle","ml"</p>
+        <AlertComponent v-if="errorEdit?.unit">
+          {{ errorEdit.unit[0] }}
+        </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Expire Date</legend>
+        <input
+          type="date"
+          class="input w-full"
+          placeholder="Expire date"
+          v-model="editForm.expireDate"
+        />
+        <AlertComponent v-if="errorEdit?.expireDate">
+          {{ errorEdit.expireDate[0] }}
+        </AlertComponent>
+      </fieldset>
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Note</legend>
+        <input type="text" class="input w-full" placeholder="Note" v-model="editForm.note" />
+        <AlertComponent v-if="errorEdit?.note">
+          {{ errorEdit.note[0] }}
+        </AlertComponent>
+      </fieldset>
+      <!-- select category -->
+      <SelectCategoryComponent v-model="selectedCategoryId" />
+      <AlertComponent v-if="errorEdit?.category">
+        {{ errorEdit.category[0] }}
+      </AlertComponent>
 
-        <!-- select user -->
-        <fieldset class="fieldset w-full">
-          <legend class="fieldset-legend">Select User</legend>
-          <select class="select w-full" placeholder="Select user" v-model="editForm.user">
-            <option v-for="(user, i) in users" :key="i" :value="user._id">
-              {{ user.username }}
-            </option>
-          </select>
-          <AlertComponent v-if="errorEdit?.user">
-            {{ errorEdit.user[0] }}
-          </AlertComponent>
-        </fieldset>
-        <div class="mt-2 text-right">
-          <button type="submit" class="btn btn-info btn-sm">Update</button>
-        </div>
-      </form>
-    </div>
-  </dialog>
+      <!-- select user -->
+      <fieldset class="fieldset w-full">
+        <legend class="fieldset-legend">Select User</legend>
+        <select class="select w-full" placeholder="Select user" v-model="editForm.user">
+          <option v-for="(user, i) in users" :key="i" :value="user._id">
+            {{ user.username }}
+          </option>
+        </select>
+        <AlertComponent v-if="errorEdit?.user">
+          {{ errorEdit.user[0] }}
+        </AlertComponent>
+      </fieldset>
+      <div class="mt-2 text-right">
+        <button type="submit" class="btn btn-info btn-sm">Update</button>
+      </div>
+    </form>
+  </ModalDialog>
 </template>
